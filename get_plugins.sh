60c523bf5d3c7000bb2af9db8cd60513cf669d1b
Ruslan Khozinov <rkhozinov@mirantis.com>
2016-04-21 18:45:18 +0300

    New version of getting plugins is added

---
+++ get_plugins.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+JENKINS_HOST=<host:port>
+curl -k -sSL "http://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/'
